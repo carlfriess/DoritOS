5dfe0a8d74dcc1b669ec55e66ee235b267f62765
Carl Friess <carl.friess@me.com>
2017-11-22 23:41:11 +0100

    Created URPC module (again)

---
+++ include/aos/urpc.h
@@ -0,0 +1,13 @@
+//
+//  urpc.h
+//  DoritOS
+//
+//  Created by Carl Friess on 22/11/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+#ifndef urpc_h
+#define urpc_h
+
+
+#endif /* urpc_h */
---
+++ lib/aos/urpc.c
@@ -0,0 +1,11 @@
+//
+//  urpc.c
+//  DoritOS
+//
+//  Created by Carl Friess on 22/11/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "aos/urpc.h"
