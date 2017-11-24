4b3ae67fb92ae74efad94029dff4c736b0eda069
Sebastian Winberg <winbergs@student.ethz.ch>
2017-11-24 05:00:25 +0100

    Added test to demonstrate really long module name spawning

--- usr/init/main.c
+++ usr/init/main.c
@@ -231,9 +231,24 @@ int main(int argc, char *argv[])
             debug_printf("%s\n", err_getstring(err));
         }
         
-        // Free the process info for memeater
+        // Free the process info
         free(si);
-
+        
+        
+        // Demonstrate that spawning processes with very long module names works
+        
+        // Allocate spawninfo
+        struct spawninfo *really_long_si = (struct spawninfo *) malloc(sizeof(struct spawninfo));
+        
+        // Spawn really_long_module_name_such_that_it_will_use_spawn_long to demonstrate SpawnLong
+        err = spawn_load_by_name("really_long_module_name_such_that_it_will_use_spawn_long", really_long_si);
+        if(err_is_fail(err)) {
+            debug_printf("%s\n", err_getstring(err));
+        }
+        
+        // Free the process info
+        free(really_long_si);
+        
     }
     
 
