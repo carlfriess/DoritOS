8a457669456b6156718b80b2b38b70de7165d25e
Carl Friess <carl.friess@me.com>
2017-12-18 23:58:25 +0100

    Added udp_socket library

---
+++ include/net/udp_socket.h
@@ -0,0 +1,31 @@
+//
+//  udp_socket.h
+//  DoritOS
+//
+//  Created by Carl Friess on 18/12/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+#ifndef udp_socket_h
+#define udp_socket_h
+
+#include <net/common.h>
+
+#include <stdint.h>
+
+
+struct udp_socket {
+    struct udp_socket_common pub;
+    struct urpc_chan chan;
+};
+
+
+// Open a UDP socket and optionally bind to a specific port
+//  Specify port 0 to bind on random port.
+errval_t socket(struct udp_socket *socket, uint16_t port);
+
+// Blockingly receive a UDP packet on the given socket
+errval_t recvfrom(struct udp_socket *socket, void *buf, size_t len,
+                  size_t *ret_len, uint32_t *from_addr, uint16_t *from_port);
+
+#endif /* udp_socket_h */
---
+++ lib/net/udp_socket.c
@@ -0,0 +1,128 @@
+//
+//  udp_socket.c
+//  DoritOS
+//
+//  Created by Carl Friess on 18/12/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+#include <net/common.h>
+#include <net/udp_socket.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <aos/aos.h>
+#include <aos/aos_rpc.h>
+
+
+// Bind to networkd
+static errval_t bind_networkd(struct urpc_chan *chan) {
+    
+    errval_t err;
+    
+    // Find networkd's pid
+    domainid_t pid = 0;
+    err = aos_rpc_process_get_pid_by_name("networkd", &pid);
+    if (err_is_fail(err)) {
+        free(chan);
+        chan = NULL;
+        return err;
+    }
+    
+    // Try to bind to networkd
+    //  Use LMP when on core 0!
+    err = urpc_bind(pid, chan, !disp_get_core_id());
+    if (err_is_fail(err)) {
+        free(chan);
+        chan = NULL;
+        return err;
+    }
+    
+    return SYS_ERR_OK;
+    
+}
+
+// Open a UDP socket and optionally bind to a specific port
+//  Specify port 0 to bind on random port.
+errval_t socket(struct udp_socket *socket, uint16_t port) {
+    
+    errval_t err;
+    
+    // Bind to networkd
+    err = bind_networkd(&socket->chan);
+    if (err_is_fail(err)) {
+        return err;
+    }
+    
+    // Set port the socket should bind to
+    socket->pub.port = port;
+    
+    // Send URPC messages
+    err = urpc_send(&socket->chan,
+                    (void *) &socket->pub,
+                    sizeof(struct udp_socket_common),
+                    URPC_MessageType_SocketOpen);
+    if (err_is_fail(err)) {
+        return err;
+    }
+    
+    // Wait for response
+    void *buf;
+    size_t size;
+    urpc_msg_type_t msg_type;
+    err = urpc_recv_blocking(&socket->chan, &buf, &size, &msg_type);
+    if (err_is_fail(err)) {
+        return err;
+    }
+    
+    if (msg_type == URPC_MessageType_SocketOpen) {
+        assert(size == sizeof(struct udp_socket_common));
+        memcpy(&socket->pub, buf, size);
+        free(buf);
+        return SYS_ERR_OK;
+    }
+    else if (msg_type == URPC_MessageType_Error) {
+        return NET_ERR_SOCKET_OPEN;
+    }
+    else {
+        return NET_ERR_INVALID_URPC;
+    }
+    
+}
+
+// Blockingly receive a UDP packet on the given socket
+errval_t recvfrom(struct udp_socket *socket, void *buf, size_t len,
+                  size_t *ret_len, uint32_t *from_addr, uint16_t *from_port) {
+    
+    // Receive a packet over URPC
+    struct udp_urpc_packet *packet;
+    size_t size;
+    urpc_msg_type_t msg_type;
+    errval_t err = urpc_recv_blocking(&socket->chan, (void **) &packet, &size,
+                                      &msg_type);
+    if (err_is_fail(err)) {
+        return err;
+    }
+    
+    // Check message type
+    if (msg_type == URPC_MessageType_Receive) {
+        
+        // Extract data
+        *from_addr = packet->addr;
+        *from_port = packet->port;
+        *ret_len = size - sizeof(struct udp_urpc_packet);
+        memcpy(buf, packet->payload, *ret_len);
+        free(packet);
+        return SYS_ERR_OK;
+        
+    }
+    else {
+        
+        return NET_ERR_INVALID_URPC;
+        
+    }
+    
+}
+
