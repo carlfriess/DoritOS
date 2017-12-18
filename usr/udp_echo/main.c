dc2c12e09bcffe3e7a6ab4f54956ffcaf3d49b35
Carl Friess <carl.friess@me.com>
2017-12-18 23:59:40 +0100

    Added udp_echo

---
+++ usr/udp_echo/main.c
@@ -0,0 +1,54 @@
+//
+//  main.c
+//  DoritOS
+//
+//  Created by Carl Friess on 15/12/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include <aos/aos.h>
+
+#include <net/udp_socket.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    errval_t err;
+    
+    struct udp_socket s;
+    
+    err = socket(&s, 80);
+    if (err_is_fail(err)) {
+        debug_printf("%s\n", err_getstring(err));
+        return 1;
+    }
+    
+    debug_printf("Socket open!\n");
+    
+    while (true) {
+                
+        char buf[32];
+        uint32_t from_addr;
+        uint16_t from_port;
+        size_t ret_size;
+        
+        err = recvfrom(&s,
+                       (void *) buf,
+                       sizeof(buf),
+                       &ret_size,
+                       &from_addr,
+                       &from_port);
+        if (err_is_fail(err)) {
+            debug_printf("%s\n", err_getstring(err));
+            return 1;
+        }
+        
+        printf("Recieved on socket: %s\n", buf);
+        
+    }
+    
+    return 0;
+    
+}
